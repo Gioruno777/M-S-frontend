d5503c4aa332acd79ff9202be084a2020052444d
root <61047079s@gapps.ntnu.edu.tw>
2025-04-27 10:23:36 +0000

    fixed:unread

--- src/tests/Food/SignUpForm.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { BrowserRouter } from 'react-router-dom'
-
-const renderWithRouter = (ui: React.ReactElement) => {
-    return render(<BrowserRouter>{ui}</BrowserRouter>)
-}
